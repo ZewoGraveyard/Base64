ed47d18273f97c1e54f2dcd59d94d108512b2ea5
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:48:35 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Base64",
     dependencies: [
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5),
     ]
 )
